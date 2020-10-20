63a0b1049d2554585533d894c2bfc2343791498c
Soufiane Ez Zine <ezzine.soufiane@gmail.com>
2020-10-20 17:24:43 +0400

    Updates

---
+++ precache-manifest.19bb86de6d5f24f9ba1fb6809c7572fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edafd4992f553e14476ba0ac658b7af",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "5ca7db10f552981d91c3",
+    "url": "/course-directory/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "a75fa17049579b0ba15f",
+    "url": "/course-directory/static/js/2.5727056d.chunk.js"
+  },
+  {
+    "revision": "b73db60337a63448fc0f0ed577e37ab5",
+    "url": "/course-directory/static/js/2.5727056d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca7db10f552981d91c3",
+    "url": "/course-directory/static/js/main.bbabece8.chunk.js"
+  },
+  {
+    "revision": "0f2fb4545997293635fb",
+    "url": "/course-directory/static/js/runtime-main.ffc6bdb4.js"
+  }
+]);
